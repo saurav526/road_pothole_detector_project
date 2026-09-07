17177b1a3482a9cb3bf0d2990bc3b1eab2d4d464
saurav526 <sauravtiwari2712005@gmail.com>
2026-09-07 11:58:43 +0530

    pandas implemention

---
+++ main.py
@@ -0,0 +1,44 @@
+import requests
+import pandas as pd
+
+url = "https://api.open-meteo.com/v1/forecast"
+
+params = {
+    "latitude": 18.52,
+    "longitude": 73.85,
+    "current": "temperature_2m,wind_speed_10m"
+}
+
+response = requests.get(url, params=params)
+
+data = response.json()
+print(data)
+
+df = pd.DataFrame([data["current"]])
+
+print(df)
+
+print(data["latitude"])
+print(data["longitude"])
+print(data["current"]["temperature_2m"])
+print(data["current"]["wind_speed_10m"])
+
+
+try:
+    response = requests.get(url, params=params, timeout=10)
+    response.raise_for_status()
+
+    data = response.json()
+    print(data)
+
+except requests.exceptions.Timeout:
+    print("Request timed out")
+
+except requests.exceptions.ConnectionError:
+    print("Connection error")
+
+except requests.exceptions.HTTPError:
+    print("API request failed")
+
+except Exception as e:
+    print("Error:", e)
